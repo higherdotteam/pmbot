597e145b6099e497ce8944a39afbd02fbe9ae55d
A Arrow <oneone@gmail.com>
2017-03-10 21:54:24 +0000

    contains

--- main.go
+++ main.go
@@ -55,24 +55,10 @@ func process(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	fmt.Println(sc.suser.Profile.Email)
+	fmt.Fprintf(w, "Processing...")
 
-	/*
-
-		team_id T035N23CL
-		team_domain mooon
-		channel_id D1KD59XH9
-		channel_name directmessage
-		user_id U035LF6C1
-		user_name aa
-		command %2Fpmbot
-		text wefwef
-
-			token=FRZTqnWNkEWVWS0YDWEA8LYV&team_id=T035N23CL&team_domain=mooon&channel_id=D1KD59XH9&channel_name=directmessage&user_id=U035LF6C1&user_name=aa&command=%2Fpmbot&text=wefwef&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT035N23CL%2F153490741014%2FX4k1pHZ79iQnX7E0apdW4Aes
-
-			token=FRZTqnWNkEWVWS0YDWEA8LYV&team_id=T035N23CL&team_domain=mooon&channel_id=G3KGJUX7A&channel_name=privategroup&user_id=U035LF6C1&user_name=aa&command=%2Fpmbot&text=here&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT035N23CL%2F152849098884%2F6Wxo8D86YYSXRHdUx9yg2rQb
-
-	*/
-	fmt.Fprintf(w, "Hello astaxie!")
+	if strings.Contains(sc.text, "fscore") {
+	}
 }
 
 func main() {
