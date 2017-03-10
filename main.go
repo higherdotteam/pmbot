33181300bd959aeeb02e8decd32a4b3d92433874
A Arrow <oneone@gmail.com>
2017-03-10 19:13:12 +0000

    http

--- main.go
+++ main.go
@@ -2,22 +2,19 @@ package main
 
 import "fmt"
 import "net/http"
-import "strings"
+
+import "io/ioutil"
 
 func process(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
+
+	b, _ := ioutil.ReadAll(r.Body)
+	fmt.Println("path", r.URL.Path, string(b))
+	fmt.Fprintf(w, "Hello astaxie!")
 }
 
 func main() {
 	http.HandleFunc("/pmbot", process)
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		panic(err)
 	}
