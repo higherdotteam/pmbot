7a219879521c799c3ec18eb97d148448adf13458
A Arrow <oneone@gmail.com>
2017-03-10 19:40:24 +0000

    string

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import "fmt"
 import "net/http"
-
+import "strings"
 import "io/ioutil"
 
 func process(w http.ResponseWriter, r *http.Request) {
 
 	b, _ := ioutil.ReadAll(r.Body)
+	data := string(b)
+	for _, v := range strings.Split(data, "&") {
+		tokens := strings.Split(v, "=")
+		fmt.Println(tokens[0], tokens[1])
+	}
 	/*
 
 		token=FRZTqnWNkEWVWS0YDWEA8LYV&team_id=T035N23CL&team_domain=mooon&channel_id=D1KD59XH9&channel_name=directmessage&user_id=U035LF6C1&user_name=aa&command=%2Fpmbot&text=wefwef&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT035N23CL%2F153490741014%2FX4k1pHZ79iQnX7E0apdW4Aes
